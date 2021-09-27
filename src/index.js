d7b7e0495dce42ca4a1dd43795093cb777ea213c
zdurr22 <zdurr22@gmail.com>
2021-09-27 15:53:36 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const taskForm = document.getElementById("create-task-form");
+  const taskDescription = document.getElementById("new-task-description");
+  const taskPriority = document.getElementById("new-task-priority");
+  const taskUl = document.getElementById("tasks");
+
+  
+  taskForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+  });
 });
